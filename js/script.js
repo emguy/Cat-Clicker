07bf12d0a6e201203cb28880381dc851263391ca
emguy <emguy.yuzhang@gmail.com>
2016-03-14 15:28:20 -0400

    Update with the new requirement.

--- js/script.js
+++ js/script.js
@@ -1,66 +1,102 @@
 (function() {
 
-var nCats = 6;
-var cat = function(url) {
-  this.clicks = 0;
-  this.url = url;
-};
-
-var activeIndex = 0;
-
-var listOfCats = [];
-
-var init = function() {
-  for (var i = 0; i < nCats; ++i) {
-    if (i % 2 == 0) {
-      var cur = new cat("http://cdnstatic.visualizeus.com/thumbs/41/35/animals,cats,cute,orange,cutecat,cat-4135b0084db66cc32f11683920dcfa1f_h.jpg");
-    } else {
-      var cur = new cat("http://cdnstatic.visualizeus.com/thumbs/31/d1/animal,cute-31d10352a0cd22c8c7a85d818937799f_h.jpg");
+/* ======= Model ======= */
+var model = {
+  currentCat: null,
+  cats: [
+    {
+      clickCount : 0,
+      name : "Tabby",
+      imgSrc : "http://i1.wp.com/www.japancrush.com/wp-content/uploads/2015/05/man-drags-cat-behind-car-kills-it-japan-02.jpg",
+    },
+    {
+      clickCount : 0,
+      name : "Tiger",
+      imgSrc : "http://www.petfoodindustry.com/ext/resources/cat-600x400.jpg",
+    },
+    {
+      clickCount : 0,
+      name : "Scaredy",
+      imgSrc : "https://shechive.files.wordpress.com/2015/11/cat-owner-struggles-101.jpg?quality=80&strip=info",
+    },
+    {
+      clickCount : 0,
+      name : "Shadow",
+      imgSrc : "http://www.chagrinfallspetclinic.com/wp-content/uploads/2014/04/cat-600x400.jpg",
+    },
+    {
+      clickCount : 0,
+      name : "Sleepy",
+      imgSrc : "http://tailandfur.com/wp-content/uploads/2016/01/30-Cute-Smiling-Cat-Pictures-8.jpg",
     }
-    listOfCats.push(cur);
-  }
-
-  renderButton();
-
-  for (var i = 0; i < nCats; ++i) {  
-    var listOfButtons = document.getElementsByClassName("cat-btn");
-    var btn = listOfButtons[i];
-    var cur = listOfCats[i];
-    console.log(btn);
-    btn.addEventListener("click", (function(curCopy) {
-      return function() {renderCat(curCopy);}
-    })(cur), false);
-    var image = document.getElementById("cat-image");
-  }
-
-  if (listOfCats.length > 0) {
-    renderCat(listOfCats[activeIndex]);
-  }
+  ]
 };
 
-//var incClicks = function(cur) {
-//  //console.log(cur);
-//  ++cur.clicks;
-//}
+/* ======= Octopus ======= */
+var octopus = {
+  init: function() {
+    model.currentCat = model.cats[0];
+    buttonView.init();
+    catView.init();
+  },
+  getCurrentCat: function() {
+    return model.currentCat;
+  },
+  incCounter: function() {
+    ++model.currentCat.clickCount;
+  },
+  getCats: function() {
+    return model.cats;
+  },
+  setCurrentCat: function(cat) {
+    model.currentCat = cat;
+  },
+};
 
-var renderButton = function() {
-  var control = document.getElementById("control-group");
-  for (var i = 0; i < nCats; ++i) {
-    var cur = document.createElement("button"); 
-    cur.className = "cat-btn";
-    cur.innerHTML = "cat " + (i + 1).toString();
-    control.appendChild(cur);
+/* ======= Cat View ======= */
+var catView = {
+  init: function() {
+    var cat = octopus.getCurrentCat();
+    this.elemImg = document.getElementById("cat-image");
+    this.elemName = document.getElementById("cat-name");
+    this.elemOut = document.getElementById("out");
+    this.elemImg.addEventListener("click", (function(catCopy) {
+      return function() {
+        octopus.incCounter(catCopy);
+        catView.render();
+      }
+    })(cat));
+    catView.render();
+  },
+  render: function() {
+    var curCat = octopus.getCurrentCat();
+    this.elemImg.src =  curCat.imgSrc;
+    this.elemName.textContent = curCat.name;
+    this.elemOut.textContent = curCat.clickCount.toString();
+  },
+}
+
+/* ======= Button View ======= */
+var buttonView = {
+  init: function() {
+    var cats = octopus.getCats();
+    var elemCats = document.getElementById("cat-list");
+    for (var i = 0; i < cats.length; ++i) {
+      var cur = document.createElement("li");
+      var curCat = cats[i];
+      cur.className = "cat-btn";
+      cur.textContent = curCat.name;
+      elemCats.appendChild(cur);
+      cur.addEventListener("click", (function(catCopy) {
+        return function() {
+          octopus.setCurrentCat(catCopy);
+          catView.render();
+        };
+      })(curCat));
+    }
   }
-};
+}
 
-var renderCat = function() {
-  var cur = listOfCats[activeIndex];
-  var image = document.getElementById("cat-image");
-  image.setAttribute("src", cur.url);
-  out.innerHTML = cur.clicks.toString();
-};
-
-init();
-
+octopus.init();
 })()
 
